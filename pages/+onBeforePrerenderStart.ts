043a4636a6e0c0c016deffa44f5299709bd10b73
yuval <yuval@example.com>
2026-02-09 23:45:04 +0200

    Deduplicate prerender URLs in hook

--- pages/+onBeforePrerenderStart.ts
+++ pages/+onBeforePrerenderStart.ts
@@ -7,5 +7,6 @@ export default function onBeforePrerenderStart() {
     .map((category) => `/category/${category.id}`);
 
   // Don't include '/' here: Vike already prerenders filesystem routes such as /pages/+Page.tsx.
-  return [...articleUrls, ...categoryUrls];
+  // Also dedupe in case content contains repeated slugs/categories.
+  return Array.from(new Set([...articleUrls, ...categoryUrls]));
 }
